7502e1b5289f5b38917d96e38e4f88f4417a268b
who <loot04@naver.com>
2022-06-21 21:30:52 +0900

    stack solution 3

--- stack.js
+++ stack.js
@@ -20,7 +20,40 @@ function solution(s) {
     return stack.length === 0
 }
 
-console.log(solution(s1))
+function solution2(s) {
+    const stack = [];
+
+    for (const c of s) {
+        if (c === '(') {
+            stack.push(c);
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return stack.length === 0
+}
+
+function solution3(s) {
+    let count = 0;
+    for (const c of s) {
+        if (c === '(') {
+            count += 1;
+        } else {
+            if (count === 0) {
+                return false;
+            }
+            count += 1;
+        }
+    }
+    return count === 0
+}
+
+console.log(solution2(s1))
+console.log(solution3(s1))
 console.log(solution(s2))
 console.log(solution(s3))
-console.log(solution(s4))
+console.log(solution2(s4))
+console.log(solution3(s4))
